ddcf117296b011f8de639f1ab31b44be4f1d2ff0
depish1 <michal.depa@rsqtechnologies.com>
2023-04-11 00:31:56 +0200

    add ThemeProvider and GlobalStyles into App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { ThemeProvider } from 'styled-components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+import { GlobalStyles } from 'Styles/GlobalStyles';
+import { theme } from 'Styles/themes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
