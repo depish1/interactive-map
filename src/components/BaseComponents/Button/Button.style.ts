d5127811d7814dcbe110e62a5767a6cd2ccb08b3
depish1 <michal.depa@rsqtechnologies.com>
2023-04-11 17:55:00 +0200

    add danger variant of button (red)

--- src/components/BaseComponents/Button/Button.style.ts
+++ src/components/BaseComponents/Button/Button.style.ts
@@ -1,13 +1,34 @@
-import styled, { css } from 'styled-components';
+import styled, { DefaultTheme, FlattenInterpolation, FlattenSimpleInterpolation, ThemeProps, css } from 'styled-components';
+import { ButtonVariantType } from './Button.type';
 
 interface IStyledButtonProps {
   fullWidth: boolean;
+  variant: ButtonVariantType;
 }
+
+const getColorForButton: Record<ButtonVariantType, FlattenInterpolation<ThemeProps<DefaultTheme>>> = {
+  default: css`
+    background-color: ${({ theme }) => theme.colors.primary1};
+    border: 0.3rem solid ${({ theme }) => theme.colors.primary1};
+
+    &:hover:not([disabled]) {
+      background-color: ${({ theme }) => theme.colors.primary2};
+      border-color: ${({ theme }) => theme.colors.primary2};
+    }
+  `,
+  danger: css`
+    background-color: ${({ theme }) => theme.colors.danger1};
+    border: 0.3rem solid ${({ theme }) => theme.colors.danger1};
+
+    &:hover:not([disabled]) {
+      background-color: ${({ theme }) => theme.colors.danger2};
+      border-color: ${({ theme }) => theme.colors.danger2};
+    }
+  `,
+};
 
 export const StyledButton = styled.button<IStyledButtonProps>`
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.primary1};
-  border: 0.3rem solid ${({ theme }) => theme.colors.primary1};
   border-radius: 1rem;
   box-shadow: ${({ theme }) => theme.boxShadows.element};
   color: ${({ theme }) => theme.colors.white1};
@@ -20,17 +41,11 @@ export const StyledButton = styled.button<IStyledButtonProps>`
   padding: 1rem 2rem;
   transition: 0.1s all ease-in;
   width: ${({ fullWidth }) => (fullWidth ? '100%' : 'fit-content')};
+  ${({ variant }) => getColorForButton[variant]}
 
   &:disabled {
     background-color: ${({ theme }) => theme.colors.white3};
     border-color: ${({ theme }) => theme.colors.white3};
-    color: ${({ theme }) => theme.colors.white1};
     cursor: default;
-  }
-
-  &:hover:not([disabled]) {
-    background-color: ${({ theme }) => theme.colors.primary2};
-    border-color: ${({ theme }) => theme.colors.primary2};
-    color: ${({ theme }) => theme.colors.white1};
   }
 `;
