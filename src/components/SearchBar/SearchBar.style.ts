86f2814d4ade172e4a4e063d5b4ab711a6e13e8d
depish1 <michal.depa@rsqtechnologies.com>
2023-04-11 03:22:04 +0200

    fix Searchbar styles

--- src/components/SearchBar/SearchBar.style.ts
+++ src/components/SearchBar/SearchBar.style.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Form = styled.form`
   background: ${({ theme }) => theme.colors.white1};
   border-radius: 0.5rem;
-  box-shadow: ${({ theme }) => theme.boxShadows.containerShadow};
+  box-shadow: ${({ theme }) => theme.boxShadows.container};
   display: grid;
   gap: 1rem;
   grid-template-columns: 1fr min-content;
